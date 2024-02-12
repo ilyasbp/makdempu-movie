fe002814161c090d238ab2fd1fe129acdfbc4612
Ilyas Bintang Prayogi <ilyas@akupintar.id>
2024-02-12 18:38:59 +0700

    Create project

---
+++ Makdempu Movie/Makdempu Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Makdempu Movie
+//
+//  Created by Ilyas Bintang Prayogi on 12/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
